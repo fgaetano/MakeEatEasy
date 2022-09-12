48ced30199354541ee585f576538067f772cdb86
fgaetano <f.gaetano90@gmail.com>
2022-09-12 13:40:53 +0000

    Getting used with doc strings and module import/usage

--- InputValidator.py
+++ InputValidator.py
@@ -1,12 +1,14 @@
+"""Helper class to validate user input"""
 class InputValidator:
   def __init__(self):
         pass
-  supported_cousines = ["Italian", "Indian", "Chinise"]
+  supported_cousines = ("italian", "indian", "thai")
 
   @staticmethod
   def ValidateInputCousine( cousine:str ) -> bool:
+    """Validate the input cousine is included in the supported ones.
+    Returns true if the input is supported, false otherwise."""
     if cousine in InputValidator.supported_cousines:
       return True
-    else:
-      return False
+    return False
     
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from InputValidator import InputValidator
 
 def main():
-  validator = InputValidator()
   foodType = input("What would you like to eat?")
-  if validator.ValidateInputCousine( foodType ):
+  if InputValidator.ValidateInputCousine( foodType ):
     print("There we go")
   else:
     print("Try again")
