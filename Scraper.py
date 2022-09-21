313b5e8cec504c502686f0b0ec93e960c51f732f
fgaetano <f.gaetano90@gmail.com>
2022-09-21 14:33:46 +0000

    - Get user input list of ingredients - Select tags from website and print out recipes name and link

--- Scraper.py
+++ Scraper.py
@@ -3,23 +3,45 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+Separator = "%20"
 
-class InputValidator:
+
+class Scraper:
 
     def __init__(self):
         pass
 
 
-def Start(cousine: str):
+def link(uri, label=None):
+    if label is None:
+        label = uri
+    parameters = ''
+
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+
+    return escape_mask.format(parameters, uri, label)
+
+
+def Start(cousine: str, keywords: list):
     conf = FileReader.LoadConfiguration()
-  
+
     websites = FileReader.GetWebSitesList()
     for url in websites:
         print("sending request to " + url)
         searchRequest = url + f"/search/results/?search={cousine}"
+        for kw in keywords:
+            searchRequest += Separator + kw
         httpResponse = requests.get(searchRequest)
         print(httpResponse.status_code)
         soup = BeautifulSoup(httpResponse.text, 'html.parser')
-        tags = soup.find_all('a', {'class': re.compile('.*card__titleLink.*')})
+        divs = soup.find_all(
+            'div', {'class': re.compile('.*detailsContainer(?=.*[aA-zZ])')})
         for i in range(conf["MaxResNumDisplayed"]):
-            print(tags[i].get('title'))
+            if i == len(divs):
+              break
+            tag = divs[i].find('a')
+            if tag is None:
+              continue
+            print(f"{i+1}: "+ tag.get('title') + "-->" + tag.get('href')+"\n")
+
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from InputValidator import InputValidator
 import Scraper
 
-def main():
-  foodType = input("What would you like to eat? ")
-  if InputValidator.ValidateInputCousine( foodType ):
-    Scraper.Start(foodType)
-  else:
-    print("Invalid input. Terminating.")
-  
 
-  
+def main():
+    foodType = input("What would you like to eat? ")
+    if InputValidator.ValidateInputCousine(foodType):
+        kws = list()
+        print("What do you have in your fridge? (insert 's' to terminate")
+        kw = input()
+        while kw != "s":
+            kws.append(kw)
+            kw = input()
+        Scraper.Start(foodType, kws)
+
+    else:
+        print("Invalid cousine. Terminating.")
+
 
 if __name__ == "__main__":
     main()
